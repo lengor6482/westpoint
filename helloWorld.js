e1443f7fd2a1a8026bf6f044ea0e629bb40c988d
lengor6482 <lengorlansana@yahoo.com>
2024-07-20 23:45:49 -0700

    commit all react files

---
+++ helloWorld.js
@@ -0,0 +1,14 @@
+const greet = (name) => {
+    console.log("Hello", name)
+}
+
+greet("Amie");
+greet("Jabu")
+
+const name = "Lansanaamaralengor";
+
+const myArray = ["Please", "push", "me", "like", "kush"]
+for (var x = 0; x < myArray.length; x++ ) {
+    const name = "lengorlansana";
+    console.log(myArray[x]);
+}
